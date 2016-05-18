2ef9345609298e79d1fc9fdb0836e1d40033e705
drkchiloll <samwomack_@me.com>
2016-05-18 08:17:31 -0500

    Added Angular Router

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES, Routes, ROUTER_PROVIDERS } from '@angular/router';
+
 import { TubeComponent } from './tube.component';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login.component';
 
 @Component({
   selector: 'app',
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css'],
-  directives: [TubeComponent]
+  directives: [TubeComponent,
+               ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
+
+@Routes([{
+  path: '/',
+  component: HomeComponent
+}, {
+  path: '/home',
+  component: HomeComponent
+}, {
+  path: '/login',
+  component: LoginComponent
+}])
 
 export class AppComponent {
   pageTitle: string;
---
+++ app/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+  template: (
+    `<p>Welcome to the Angular 2 Educational App</p>`
+  ),
+  directives: [ROUTER_DIRECTIVES]
+})
+
+export class HomeComponent {}
